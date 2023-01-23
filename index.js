36ce85d6cfdc4f95d30fa7c4c229fa1b5a6d6b23
Artur Levin <u-50@tptlive.ee>
2023-01-23 15:38:20 +0200

    Add express server and nodemon

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello Node!");
+const express = require('express');
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
